49ed8c982902d85513af499256a5c853d077fa51
Gagan Deep Singh <gagangulyanig@gmail.com>
2021-08-17 01:26:03 +0530

    Add Quiz #1 on Arithmetic Operators

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Python-Quizzes
 This repo contains Python Quizzes (with solutions) that I post on Twitter
+
+## Topics:
+<hr>
+
+### 1. Arithmetic Operators
+
+- [Python Quiz #1](./arithmetic-1.py)
---
+++ arithmetic-1.py
@@ -0,0 +1,28 @@
+"""
+Can you guess the output of
+the following Python Snippet?
+
+Options
+=========
+
+1. 1000
+2. 10
+3. -10
+4. SyntaxError
+"""
+
+print(100 // 10 + 100 - 10 * 10)
+
+"""
+Solution
+=========
+
+2. 10
+
+Explaination:
+    -> (100 // 10) + 100 - (10 * 10)
+    -> 10 + 100 - 100
+    -> 10
+
+For more Pythonic Content, find me on Twitter @GaganGulyani
+""""
